d70e2ebcc1684073c1bf289b2290d979fa16c0bc
Abdelilah-Falih <121343981+Abdelilah-Falih@users.noreply.github.com>
2023-06-12 22:11:37 +0100

    ex06 :  -> loading users in background in a long duration(using asyncTask)  -> add progressDialog while loading users ..

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
@@ -26,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding ;
     View root;
 
-
+    ProgressDialog dialog ;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
         root = binding.getRoot();
         setContentView(root);
 
-
-
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("loading users...");
+        dialog.setCancelable(false);
 
         binding.tvSwip.setOnTouchListener(new FlingManager(this) {
             @Override
@@ -52,9 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
         binding.btnLoadUsers.setOnClickListener(v->{
-            UserAdapter adapter = new UserAdapter(this, getUsers());
-            binding.lvUsersnames.setAdapter(adapter);
+            new InBackground().execute();
+            dialog.show();
         });
 
 
@@ -69,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    ArrayList<User> getUsers(){
+    /*ArrayList<User> getUsers(){
         try (InputStream inputStream = getAssets().open("users.json")){
             int c = inputStream.read();
             StringBuilder jsonString = new StringBuilder();
@@ -94,13 +100,71 @@ public class MainActivity extends AppCompatActivity {
                         user.getString("city")
                 ));
 
+
             }
+
 
             return users_obj;
 
         } catch (IOException | JSONException e) {
             e.printStackTrace();
             return  null;
+        }
+    }*/
+
+    class InBackground extends AsyncTask<Void, ArrayList<User>, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try (InputStream inputStream = getAssets().open("users.json")){
+                int c = inputStream.read();
+                StringBuilder jsonString = new StringBuilder();
+                ArrayList<User> users_obj = new ArrayList<>();
+
+                while (c != -1){
+                    jsonString.append((char)c);
+                    c = inputStream.read();
+                }
+
+                JSONObject jsonObject = new JSONObject(jsonString.toString());
+                JSONArray users = jsonObject.getJSONArray("users");
+
+                for (int i = 0; i < users.length(); i++) {
+                    JSONObject user = users.getJSONObject(i);
+                    JSONObject names = user.getJSONObject("name");
+
+                    users_obj.add(new User(
+                            names.getString("first"),
+                            names.getString("last"),
+                            user.getString("gender"),
+                            user.getString("city")
+                    ));
+                    Thread.sleep(200);
+
+                }
+
+
+                publishProgress(users_obj);
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+                return  null;
+            } catch (InterruptedException e) {
+                Toast.makeText(MainActivity.this, "error sleep", Toast.LENGTH_SHORT).show();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(ArrayList<User>... values) {
+            UserAdapter adapter = new UserAdapter(MainActivity.this, values[0]);
+            binding.lvUsersnames.setAdapter(adapter);
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            dialog.dismiss();
         }
     }
 
